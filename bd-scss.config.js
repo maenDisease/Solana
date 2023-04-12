252065d432e18be1943dd00f5c11fb2f631026f4
maenDisease <ninomae2n@gmail.com>
2023-04-12 15:22:42 -0400

    comma

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -5,7 +5,7 @@ export default {
 		author: "maenDisease",
 		version: "1.0.11",
 		description: "macOS with customization.",
-		source: "https://github.com/maenDisease/Solana"
+		source: "https://github.com/maenDisease/Solana",
 		website: "https://maendisease.github.io/",
 		invite: "BShu37e4jg"
 	}
